6fb8b6af0a18d2b4f99671f12e0af062e84ef720
antinomy <arekbyu@gmail.com>
2024-03-21 09:24:19 -0700

    1

---
+++ src/zoosim/Animal.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zoosim;
+
+/**
+ *
+ * @author jonat
+ */
+public abstract class Animal implements IEntity {
+
+    String id;
+    String name;
+    String species;
+    boolean male;
+    int age;
+    direction posX;
+    direction posY;
+    String Size;
+    double speed;
+    double direction;
+    int hunger;
+    int fatigue;
+    Image image;
+    String sound;
+
+    @Override
+    /**
+     * prints the sound that the animal makes.
+     */
+    public void makeSound() {
+        System.out.println(sound);
+    }
+
+    /**
+     * moves animal one time step.
+     */
+    public void move() {
+        posX += speed * Math.cos(direction);
+        posY += speed * Math.sin(direction);
+    }
+
+    /**
+     * resets animal hunger.
+     */
+    public void eat() {
+        hunger = 0;
+    }
+
+    @Override
+    /**
+     * resets animal fatigue.
+     */
+    public void sleep() {
+        fatigue = 0;
+    }
+
+    /**
+     * turns the animal's movement direction (in radians).
+     *
+     * @param radians
+     */
+    public void turn(double radians) {
+        direction += radians;
+    }
+}
---
+++ src/zoosim/IObject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package zoosim;
+
+/**
+ *
+ * @author jonat
+ */
+public interface IObject {
+
+    public void turn(int degrees) {
+
+    }
+
+    public void place(int x, int y) {
+
+    }
+}
